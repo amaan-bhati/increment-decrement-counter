6bec9f436a2a934cbdeaea125e0dc292263f59f3
amaan-bhati <amaanbhati49@gmail.com>
2022-03-13 22:48:11 +0530

    final commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var input=document.getElementById("input")  
      function increase(){  
-       input.value=eval(input.value)+1  
+       input.value=eval(input.value)+1//upper limit
+       //changed different values but the functionality remained the same and hence the functionality is fit 
      }  
      function decrease(){  
-       input.value=eval(input.value)-1  
+       input.value=eval(input.value)-1//lower limit
      }  
